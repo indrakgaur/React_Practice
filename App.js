bde5915c235462ea4b30887f420f407f13d4f293
Indrakumar <indrakumar9.ig@gmail.com>
2024-06-10 16:58:03 +0530

    Day 4: component and props

--- App.js
+++ App.js
@@ -1,23 +1,358 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const headingJSX = <h1 className='test'>Namaste React!</h1>;
-
-const Title = () => {
-  return <h2 className='child-div'>Heading 2 from component</h2>;
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='logo'>
+        <img src='https://img.freepik.com/premium-vector/restaurant-logo-design_636083-178.jpg' />
+      </div>
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Cart</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-const elem = <h2>Heading 2 from element</h2>;
+const RestuarnantCard = (props) => {
+  const { resData } = props;
 
-const Heading = () => {
+  const { name, cuisines, avgRating, costForTwo, deliveryTime } = resData?.info;
+
   return (
-    <div id='container'>
-      <h1>Heading 1 Component</h1>
-      <Title />
-      {elem}
+    <div className='res-card'>
+      <img
+        className='res-logo'
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData.info.cloudinaryImageId
+        }
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{deliveryTime}</h4>
+    </div>
+  );
+};
+
+const resList = [
+  {
+    info: {
+      id: "9862",
+      name: "Pizza Hut",
+      cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+      locality: "Bandra",
+      areaName: "Bandra Kurla Complex",
+      costForTwo: "₹300 for two",
+      cuisines: ["Pizzas"],
+      avgRating: 4.1,
+      parentId: "721",
+      avgRatingString: "4.1",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 40,
+        lastMileTravel: 2.3,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "2.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-06-11 03:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Pizza.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Pizza.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/pizza-hut-bandra-bandra-kurla-complex-mumbai-9862",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "28405",
+      name: "Chinese Wok",
+      cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
+      locality: "CST Road",
+      areaName: "Santacruz East",
+      costForTwo: "₹250 for two",
+      cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
+      avgRating: 4.3,
+      parentId: "61955",
+      avgRatingString: "4.3",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 44,
+        lastMileTravel: 1.6,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "1.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-06-11 01:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Chinese.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Chinese.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹179",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/chinese-wok-cst-road-santacruz-east-mumbai-28405",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "448491",
+      name: "UBQ BY Barbeque Nation",
+      cloudinaryImageId: "e2ej0ob56z47oylq9mkb",
+      locality: "Kalina",
+      areaName: "Bandra East",
+      costForTwo: "₹300 for two",
+      cuisines: [
+        "North Indian",
+        "Barbecue",
+        "Biryani",
+        "Kebabs",
+        "Mughlai",
+        "Desserts",
+      ],
+      avgRating: 3.9,
+      parentId: "10804",
+      avgRatingString: "3.9",
+      totalRatingsString: "500+",
+      sla: {
+        deliveryTime: 44,
+        lastMileTravel: 2.5,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "2.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-06-10 23:30:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "60% OFF",
+        subHeader: "UPTO ₹120",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/ubq-by-barbeque-nation-kalina-bandra-east-mumbai-448491",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "19451",
+      name: "Havmor Havfunn Ice Cream",
+      cloudinaryImageId: "49845b494b1e637209b7bd1c7ca184ad",
+      locality: "Chembur",
+      areaName: "Chembur",
+      costForTwo: "₹400 for two",
+      cuisines: ["Ice Cream", "Ice Cream Cakes", "Desserts", "Beverages"],
+      avgRating: 4.6,
+      parentId: "94570",
+      avgRatingString: "4.6",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 36,
+        lastMileTravel: 5.9,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "5.9 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2024-06-10 23:59:00",
+        opened: true,
+      },
+      badges: {
+        textExtendedBadges: [
+          {
+            iconId: "v1705582451/Ratnesh_Badges/Perfect_cake.png",
+            shortDescription: "Perfect cake delivery",
+            fontColor: "#7E808C",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "",
+                  fontColor: "#7E808C",
+                  iconId: "v1705582451/Ratnesh_Badges/Perfect_cake.png",
+                  shortDescription: "Perfect cake delivery",
+                },
+              },
+            ],
+          },
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "₹100 OFF",
+        subHeader: "ABOVE ₹299",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/havmor-havfunn-ice-cream-chembur-mumbai-19451",
+      type: "WEBLINK",
+    },
+  },
+];
+
+const Body = () => {
+  return (
+    <div className='body'>
+      <div className='search'>Search</div>
+      <div className='res-container'>
+        {resList.map((restaurant) => (
+          <RestuarnantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className='app'>
+      <Header />
+      <Body />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Heading />);
+root.render(<AppLayout />);
